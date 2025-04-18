220088bfcf0cb85842104acfedaa3d8164850795
Tyler Lundin <TylerJamesLundin@gmail.com>
2025-04-18 12:38:54 -0600

    Fixed url in mobile menu and broken &apos;

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -81,7 +81,7 @@ export default function MobileMenu({ isOpen, onClose }: MobileMenuProps) {
               </li>
               <li>
                 <a
-                  href="#services"
+                  href="/services"
                   className="block text-lg text-black hover:text-gray-600 transition-colors duration-200"
                   onClick={onClose}
                 >
